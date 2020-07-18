b477ba56e9e5edb34fc780acc9916b6cebefc11b
onion <188869754@qq.com>
2020-07-18 18:59:13 +0800

    lc

--- leetcode/problems/0236.Lowest Common Ancestor of a Binary Tree.cpp
+++ leetcode/problems/0236.Lowest Common Ancestor of a Binary Tree.cpp
@@ -10,6 +10,7 @@
 
 class Solution {
 public:
+    // recursion
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
         if(root == nullptr) return root;
@@ -29,5 +30,46 @@ public:
         else{
             return rightChild;
         }
+    }
+
+    // Non recursion
+    TreeNode* lowestCommonAncestor2(TreeNode* root, TreeNode* p, TreeNode* q) {
+
+        vector<TreeNode*> stack, sp, sq;
+        TreeNode* cur = root;
+        TreeNode* prior = nullptr;
+        int cnt = 0;
+
+        while((cur || !stack.empty()) && cnt < 2){
+            if(cur){
+                stack.push_back(cur);
+                cur = cur->left;
+            }
+            else{
+                cur = stack.back();
+                if(cur->right && cur->right != prior){
+                    cur = cur->right;
+                }
+                else{
+                    if(cur == p){
+                        sp = vector<TreeNode*>(stack.rbegin(), stack.rend());
+                        cnt ++;
+                    }
+                    if(cur == q){
+                        sq = vector<TreeNode*>(stack.rbegin(), stack.rend());
+                        cnt ++;
+                    }
+                    stack.pop_back();
+                    prior = cur;
+                    cur = nullptr;
+                }
+            }
+        }
+        for(int i = 0; i < sp.size(); i ++){
+            for(int j = 0; j < sq.size(); j ++){
+                if(sp[i] == sq[j]) return sp[i];
+            }
+        }
+        return nullptr;
     }
 };
