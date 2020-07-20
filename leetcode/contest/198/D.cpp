1d6f2a866a04642a21219a908191995fcb6855c1
onion <188869754@qq.com>
2020-07-21 01:13:24 +0800

    lc

--- leetcode/contest/198/D.cpp
+++ leetcode/contest/198/D.cpp
@@ -1,21 +1,60 @@
-class Solution {
 public:
-    // O(n^2) Solution
+
+    // O(nlogn) Solution
     int closestToTarget(vector<int>& arr, int target) {
 
-        int res = INT_MAX;
-
+        vector<vector<int>> pos(30);
         for(int i = 0; i < arr.size(); i ++){
-            int sum = arr[i];
-            for(int j = i; j < arr.size(); j ++){
-                sum = sum & arr[j];
-                res = min(res, abs(sum - target));
-                if(res == 0) return res;
-                if(sum <= target) break;
+            for(int j = 0; j < 30; j ++){
+                if((arr[i] >> j) & 1) pos[j].push_back(i);
             }
-            if(sum >= target) break;
         }
 
-        return res;
+        int n = (int)arr.size();
+        vector<int> dp(n);
+        int ans = INT_MAX;
+        for(int i = n - 1; i >= 0; i --){
+            for(int j = 0; j < 30; j ++){
+                if(!((arr[i] >> j) & 1)){
+                    while(!pos[j].empty() && pos[j].back() > i){
+                        dp[pos[j].back()] -= (1 << j);
+                        pos[j].pop_back();
+                    }
+                }
+            }
+            dp[i] = arr[i];
+
+            auto it = lower_bound(dp.begin() + i, dp.end(), target, greater<int>());
+            if(it == dp.begin() + i){
+                ans = min(ans, abs(*it - target));
+            }
+            else if(it == dp.end()){
+                ans = min(ans, abs(*(it - 1) - target));
+            }
+            else{
+                ans = min(ans, min(abs(*it - target), abs(*(it - 1) - target)));
+            }
+        }
+        return ans;
     }
 };
+
+
+// O(n^2) Solution
+int closestToTarget(vector<int>& arr, int target) {
+
+    int res = INT_MAX;
+
+    for(int i = 0; i < arr.size(); i ++){
+        int sum = arr[i];
+        for(int j = i; j < arr.size(); j ++){
+            sum = sum & arr[j];
+            res = min(res, abs(sum - target));
+            if(res == 0) return res;
+            if(sum <= target) break;
+        }
+        if(sum >= target) break;
+    }
+
+    return res;
+}
