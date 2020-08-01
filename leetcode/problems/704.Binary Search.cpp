c26a040301383e5430f05f9acf1ddf13c931d150
onion <188869754@qq.com>
2020-08-01 17:57:26 +0800

    lc

---
+++ leetcode/problems/704.Binary Search.cpp
@@ -0,0 +1,20 @@
+class Solution {
+
+private:
+
+    // binary search the target in [l...r)
+    int binarySearch(vector<int> nums, int l, int r, int t){
+
+        if(l >= r) return -1;
+
+        int mid = l + (r - l) / 2;
+        if(t == nums[mid]) return mid;
+        return t < nums[mid] ? binarySearch(nums, l, mid, t) : binarySearch(nums, mid + 1, r, t);
+    }
+
+public:
+    int search(vector<int>& nums, int target) {
+
+        return binarySearch(nums, 0, nums.size(), target);
+    }
+};
