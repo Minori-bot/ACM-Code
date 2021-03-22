9117c686c2a7684fe7cee6f001426bc1f09c4fc9
onionyzl <17670843262@163.com>
2021-03-22 21:52:34 +0800

    881

---
+++ leetcode/problems/0881.Boats to Save People.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+
+        sort(people.begin(), people.end());
+        int res = 0;
+        int i = 0, j = people.size() - 1;
+        
+        while(i < j){
+            if(people[i] + people[j] <= limit){
+                i ++, j --, res ++;
+            }
+            else{
+                j --, res ++;
+            }
+        }
+        if(i == j) res ++;
+        
+        return res;
+    }
+};
